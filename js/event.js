44e13038ffe4a9463332b9f162a78871117a0f80
hironmoy-ray <hpray35@gmail.com>
2024-03-03 00:17:16 +0600

    Delete with Disabled Feature

--- js/event.js
+++ js/event.js
@@ -15,6 +15,7 @@ document.getElementById("bg-white").addEventListener("click", function () {
   document.body.style.backgroundColor = "white";
 });
 
+// <!-- Update Text -->
 document.getElementById("btn-update").addEventListener("click", function () {
   const inputField = document.getElementById("input-field");
   const inputText = inputField.value;
@@ -22,4 +23,17 @@ document.getElementById("btn-update").addEventListener("click", function () {
   const displayInput = document.getElementById("display-input");
   displayInput.innerText = inputText;
   inputField.value = "";
+});
+
+// <!-- Post Comment -->
+document.getElementById("btn-post").addEventListener("click", function () {
+  // console.log("Clicked By Post Btn");
+  const textField = document.getElementById("text-field");
+  const newText = textField.value;
+  // console.log(text);
+  const postContainer = document.getElementById("post-container");
+  const p = document.createElement("p");
+  p.innerText = newText;
+  postContainer.appendChild(p);
+  textField.value = "";
 });
