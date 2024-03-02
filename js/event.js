6ff0e60d69d5cceb91845c6060c5f7f8fb1df2de
hironmoy-ray <hpray35@gmail.com>
2024-03-02 23:35:54 +0600

    Posted Comment

--- js/event.js
+++ js/event.js
@@ -8,6 +8,18 @@ makeRed.onclick = function () {
   document.body.style.backgroundColor = "red";
 };
 
-document.getElementById("bg-purple").addEventListener('click',function(){
-    document.body.style.backgroundColor = 'purple';
-})
+document.getElementById("bg-purple").addEventListener("click", function () {
+  document.body.style.backgroundColor = "purple";
+});
+document.getElementById("bg-white").addEventListener("click", function () {
+  document.body.style.backgroundColor = "white";
+});
+
+document.getElementById("btn-update").addEventListener("click", function () {
+  const inputField = document.getElementById("input-field");
+  const inputText = inputField.value;
+
+  const displayInput = document.getElementById("display-input");
+  displayInput.innerText = inputText;
+  inputField.value = "";
+});
